58650326f326d7444e8b9d6d1b41579bd28d48f1
Lanbig <lanbig@msn.com>
2017-08-18 15:57:29 -0500

    scraping Products Rating and Reviews

--- lowes-scraping.py
+++ lowes-scraping.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 def lowes_search(query):
@@ -29,34 +30,94 @@ def product_list(r):
 
     print("Number of Products:", len(titles))
 
-    return titles,urls
+    return titles, urls
 
 
-def review_extract(urls):
-
-    def product_review(url):
+def reviews_scraping(url):
+    def product_review_request(url):
         with requests.session() as c:
             url = url
-            url_link = requests.get(url + '/reviews')
+            url_link = requests.get(url)
             print(url_link.url)
 
             return url_link
 
-    pp = product_review(urls[0])
+    def product_review_extract(soup):
+        # extract part
+        review_titles = soup.find_all('h4', class_='reviews-list-quote grid-60 clearfix v-spacing-medium')
+        reviews = soup.find_all('p', class_='reviews-helpful-text secondary-text v-spacing-medium')
+        ratings = soup.find_all('meta', {'itemprop': 'ratingValue'})
+
+        ext_review_titles = []
+        ext_reviews = []
+        ext_ratings = []
+
+        for item in zip(review_titles, ratings, reviews):
+            ext_review_titles.append(str(item[0].text).strip())
+            ext_ratings.append(str(item[1]['content']).strip())
+            ext_reviews.append(re.sub(r'\r', '',str(item[2].text).strip()))
+            # Matches Unicode whitespace characters (which includes [ \t\n\r\f\v],
+            # https://stackoverflow.com/questions/41896322/how-to-remove-all-kind-of-linebreaks-or-formattings-from-strings-in-python
+
+        return ext_review_titles, ext_ratings, ext_reviews
+
+    def product_review_number(soup):
+        try:
+            reviews_number = soup.find("span", class_='reviews-number h3')
+            reviews_number = re.findall(r'\d+', str(reviews_number.text))
+            reviews_number = int(reviews_number[0])
+        except:
+            print("No data")
+            return 0
+
+        print('Number of reviews: ', reviews_number)
+
+        return reviews_number
+
+    # Review collection
+    pp = product_review_request(url + '/reviews')
     soup = BeautifulSoup(pp.text, "html.parser")
 
-    reviews_number = soup.find("span", class_='reviews-number h3')
-    reviews_number = re.findall(r'\d+', str(reviews_number.text))
-    reviews_number = int(reviews_number[0])
+    # extract part
+    reviews_number = product_review_number(soup)
 
-    reviews = soup.find_all('p', class_='reviews-helpful-text secondary-text v-spacing-medium')
+    ext_review_titles = []
+    ext_reviews = []
+    ext_ratings = []
 
-    for review in reviews:
-        print(str(review.text).strip())
+    if reviews_number == 0:
+        return ext_review_titles, ext_ratings, ext_reviews
+    else:
+        ext_review_titles, ext_ratings, ext_reviews = product_review_extract(soup)
 
-### Main ###
+        if reviews_number > 10:
+            nloop = int(reviews_number / 10)
+
+            for i in range(nloop):
+                urll = (url + '/reviews?offset=' + str((i + 1) * 10))
+                pp = product_review_request(urll)
+                soup = BeautifulSoup(pp.text, "html.parser")
+
+                x, y, z = product_review_extract(soup)
+
+                ext_review_titles += x
+                ext_ratings += y
+                ext_reviews += z
+
+        return ext_review_titles, ext_ratings, ext_reviews
+
+### Main function ###
 
 r = lowes_search('samsung+washer')
 titles, urls = product_list(r)
 
-review_extract(urls)
+for product_item in zip(titles, urls):
+    review_titles, ratings, reviews = reviews_scraping(product_item[1])
+
+    product_reviews = pd.DataFrame(
+        {'review_titles': review_titles,
+         'ratings': ratings,
+         'reviews': reviews
+         })
+
+    product_reviews.to_csv('review_data/'+product_item[0]+'.csv', index=False)
